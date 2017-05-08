a2188630cc12d934c436aec4cc8fa45db1ae325f
Joram Tetelepta <joramspam@hotmail.com>
2017-05-08 15:46:52 +0200

    Bestelling enzo

--- Proftaak B22 @Life/Proftaak B22 @Life/Forms/BestellingForm.xaml.cs
+++ Proftaak B22 @Life/Proftaak B22 @Life/Forms/BestellingForm.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Proftaak_B22__Life.DatabaseContext;
+using Proftaak_B22__Life.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,59 @@ namespace Proftaak_B22__Life.Forms
     /// </summary>
     public partial class BestellingForm : Window
     {
-        public BestellingForm()
+        private BestellingSQLContext bestellingcontext = new BestellingSQLContext();
+        private MedewerkerSQLContext medewerkercontext = new MedewerkerSQLContext();
+        private List<Window> actief;
+        private int selectedid;
+
+        public BestellingForm(List<Window> actief)
         {
             InitializeComponent();
+            foreach(Bestelling b in bestellingcontext.GetOpenBestellingen())
+            {
+                lbOpen.Items.Add(b);
+            }
+            foreach (Bestelling b in bestellingcontext.GetGeslotenBestellingen())
+            {
+                lbGesloten.Items.Add(b);
+            }
+            this.actief = actief;
+        }
+
+        private void lbGesloten_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedbestelling;
+            if (lbGesloten.SelectedIndex != -1)
+            {
+                selectedbestelling = lbGesloten.SelectedItem.ToString();
+                int id = Convert.ToInt32(selectedbestelling.Split(',')[0]);
+                selectedid = id;
+                tbMedewerker.Text = medewerkercontext.GetMedewerkerByID(bestellingcontext.GetBestellingByID(id).Medewerker).LastName;
+                tbMedewerker.IsEnabled = false;
+                tbBestel.Text = bestellingcontext.GetBestellingByID(id).Besteldatum.ToString("dd/MM/yyyy");
+                tbBestel.IsEnabled = false;
+                tbKlant.IsEnabled = false;
+                tbLever.Text = bestellingcontext.GetBestellingByID(id).Leverdatum.ToString("dd/MM/yyyy");
+                tbLever.IsEnabled = false;
+                tbBetaal.Text = bestellingcontext.GetBestellingByID(id).Betaaldatum.ToString("dd/MM/yyyy");
+                tbBetaal.IsEnabled = false;                       
+              }
+           }
+
+        private void lbOpen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedbestelling;
+            if (lbOpen.SelectedIndex != -1)
+            {
+                selectedbestelling = lbOpen.SelectedItem.ToString();
+                int id = Convert.ToInt32(selectedbestelling.Split(',')[0]);
+                selectedid = id;
+                tbMedewerker.Text = medewerkercontext.GetMedewerkerByID(bestellingcontext.GetBestellingByID(id).Medewerker).LastName;
+                tbMedewerker.IsEnabled = false;
+                tbBestel.Text = bestellingcontext.GetBestellingByID(id).Besteldatum.ToString("dd/MM/yyyy");
+                tbBestel.IsEnabled = false;
+                tbKlant.IsEnabled = false;
+            }
         }
     }
-}
+    }
